f7cc949d336c51d830f4fab8b7690f3a207cf228
Sofia Charalampous <scharalampous84@gmail.com>
2026-01-09 16:31:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-756514731-a2339",
+  "appId": "1:643037213122:web:8428c1795e9dd704f1265b",
+  "apiKey": "AIzaSyA-oR5UrpI1pQEVrks7ZvvoKCZhKqytVH8",
+  "authDomain": "studio-756514731-a2339.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "643037213122"
+};
