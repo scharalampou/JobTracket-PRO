4e4bf27034d4264db663551a53969c5c1f721f01
Sofia Charalampous <scharalampous84@gmail.com>
2026-01-10 00:13:19 +0000

    Change the background color of this button to #F9624E and the color of t

--- src/components/app/close-application-dialog.tsx
+++ src/components/app/close-application-dialog.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import {
@@ -67,7 +68,12 @@ export function CloseApplicationDialog({ application }: CloseApplicationDialogPr
           <TooltipTrigger asChild>
             <DialogTrigger asChild>
               <Button variant="ghost" size="icon">
-                <Archive className="h-4 w-4" />
+                <div
+                  className="flex items-center justify-center h-6 w-6 rounded-sm"
+                  style={{ backgroundColor: '#F9624E' }}
+                >
+                  <Archive className="h-4 w-4 text-white" />
+                </div>
                 <span className="sr-only">Close Application</span>
               </Button>
             </DialogTrigger>
